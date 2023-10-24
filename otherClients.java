09f85cd6b0fb9c9873355e3d01805bab0eef7154
Gusto801 <148716270+Gusto801@users.noreply.github.com>
2023-10-24 12:19:04 +0100

    Update otherClients.java

--- otherClients.java
+++ otherClients.java
@@ -1,43 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package hotelsystem;
+package hotelSysetm;
 
-/**
- *
- * @author PC
- */
 public class otherClients {
-        String type = "other";
-        int choice; // choice 1 = Restaurant 
+        //String type = "other";
+        //int choice; // choice 1 = Restaurant 
                     //choice 2 = Bar
                     //choice 3 = Cafe
                     //choice 4 = pool
         
-        public final double restPrice = 19000.0;
-        public final double barPrice = 18000.0;
-        public final double cafePrice = 15000.0;
-        public double poolPrice;
-        public final double kidPool = 500;
-        public final double adultPool = 1000;
-        public int kids; //how many kids in the family to use the pool
-        public int adults; //how many adults in the family 
+        public final static double restPrice = 19000.0;
+        public final static double barPrice = 18000.0;
+        public final static double cafePrice = 15000.0;
+        //public double poolPrice;
+        public final static double kidPoolprice = 500;
+        public final static double adultPoolprice = 1000;
+       // public int kids; //how many kids in the family to use the pool
+       // public int adults; //how many adults in the family 
         
-        public otherClients(String type , int choice){
-            this.type = type;
+       /* public otherClients( int choice){
+          //  this.type = type;
             this.choice = choice;
-        }
+        }*/
         
-        public double servicePrice(String type , int people){
-            if(this.type.equals("other") && choice == 1)
-                return restPrice;
-            else if(this.type.equals("other") && choice == 2)
-                return barPrice;
-            else if(this.type.equals("other") && choice == 3)
-                return cafePrice;
+        public static double servicePrice(int choice, int numOfadultPersons ,int numOfkids){
+            if( choice == 1)
+                return restPrice*numOfadultPersons;
+            else if(choice == 2)
+                return barPrice*numOfadultPersons;
+            else if( choice == 3)
+                return cafePrice*numOfadultPersons;
             else{ //Pool section
-                return (kids * kidPool) + (adults * adultPool);
+                return (numOfkids * kidPoolprice) + (numOfadultPersons* adultPoolprice);
             }
         }
 }
